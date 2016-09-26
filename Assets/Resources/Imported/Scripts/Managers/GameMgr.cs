c3662d815041e8d5393737be64308cefc7777201
Enielka <a.kleine@student.isartdigital.com>
2016-09-26 17:14:38 +0200

    NbTurns and Victory
    Add system of turns and of victory

---
+++ Assets/Resources/Imported/Scripts/Managers/GameMgr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMgr : MonoBehaviour {
+
+    [SerializeField]
+    public int max_turn = 3;
+
+    private static GameMgr instance = null;
+    public static GameMgr Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            instance = GameObject.FindGameObjectWithTag("GameMgr").GetComponent<GameMgr>();
+            return instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
