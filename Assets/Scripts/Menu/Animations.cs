8bc4ad423db4f4dbea3b293b922a79331d15e825
Adrien <a.bourgois@student.isartdigital.com>
2016-09-27 16:36:39 +0200

    Menu : Add states - Fade system

--- Assets/Scripts/Menu/Animations.cs
+++ Assets/Scripts/Menu/Animations.cs
@@ -14,9 +14,17 @@ public class Animations : MonoBehaviour {
         }
     }
 
-    GameObject current_animated_object = null;
-
     MenuManager menu_mgr = null;
+
+    enum EObject_To_Display { Cars, Tracks, Selection, Any };
+    EObject_To_Display current_state = EObject_To_Display.Cars;
+    enum EDisplay_State { In, Out, Rotate, Next };
+    [SerializeField]
+    EDisplay_State current_display = EDisplay_State.Rotate;
+    [SerializeField]
+    float time_between_objects = 10f;
+    float before_next_object = 0f;
+    GameObject current_animated_object = null;
 
     void Awake()
     {
@@ -34,15 +42,59 @@ public class Animations : MonoBehaviour {
         return menu_mgr.cars[id];
     }
 
-    void Update () {
-        if(current_animated_object)
-            current_animated_object.transform.Rotate(new Vector3(0, 45 * Time.deltaTime, 0), Space.Self);
+    void Update ()
+    {
+        if (current_animated_object)
+        {
+            if (current_display == EDisplay_State.Rotate)
+            {
+                current_animated_object.transform.Rotate(new Vector3(0, 45 * Time.deltaTime, 0), Space.Self);
+            }
+            else if (current_display == EDisplay_State.In)
+            {
+                Renderer[] object_renderer = current_animated_object.GetComponentsInChildren<Renderer>();
+                foreach (Renderer renderer in object_renderer)
+                {
+                    Color object_color = renderer.material.color;
+                    if (object_color.a < 1)
+                    {
+                        object_color.a = Mathf.Lerp(object_color.a, object_color.a + 0.5f, Time.deltaTime);
+                        renderer.material.color = object_color;
+                    }
+                    else
+                    {
+                        current_display = EDisplay_State.Rotate;
+                    }
+                }
+            }
+            else if (current_display == EDisplay_State.Out)
+            {
+                Renderer[] object_renderer = current_animated_object.GetComponentsInChildren<Renderer>();
+                foreach (Renderer renderer in object_renderer)
+                {
+                    Color object_color = renderer.material.color;
+                    if (object_color.a > 0)
+                    {
+                        object_color.a = Mathf.Lerp(object_color.a, object_color.a - 0.5f, Time.deltaTime);
+                        renderer.material.color = object_color;
+                    }
+                    else
+                    {
+                        current_display = EDisplay_State.Next;
+                    }
+                }
+            }
+        }
+        else if (current_display == EDisplay_State.Next)
+        { 
+            changeObject(GetRandomCar(), new Vector3(2.3f, -0.8f, 90f));
+        }
     }
 
     public void changeObject(GameObject obj, Vector3 position)
     {
         if(current_animated_object)
-            DestroyObject(current_animated_object, 2f);
+            DestroyObject(current_animated_object);
         current_animated_object = Instantiate(obj);
         current_animated_object.transform.SetParent(menu_mgr.cam.transform);
         current_animated_object.transform.Rotate(new Vector3(25, 145, 0), Space.Self);
