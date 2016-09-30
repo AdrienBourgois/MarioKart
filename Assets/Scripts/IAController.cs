f9ec23d6b777622a0354c7b8b90a5f36733c2e93
Enielka <a.kleine@student.isartdigital.com>
2016-09-30 12:44:48 +0200

    Update path IA

--- Assets/Scripts/IAController.cs
+++ Assets/Scripts/IAController.cs
@@ -13,11 +13,12 @@ public class IAController : MonoBehaviour {
     [SerializeField]
     private GameMgr gameMgr;
 
+    private float max_pos = 10f;
     private float reach_distance = 5.0f;
     private Vector3 current_point;
     private Vector3 last_point;
     private int current_way_point_id = 0;
-    private ICarController controller = null;
+    private ICarController controller;
 
     void Awake()
     {
@@ -36,8 +37,9 @@ public class IAController : MonoBehaviour {
         if (gameMgr.game_ready == true)
         {
             controller.Move(0f, 1f, 0f, 0f);
-            float distance = Vector3.Distance(path.points[current_way_point_id].position, transform.position);
-            transform.position = Vector3.MoveTowards(transform.position, path.points[current_way_point_id].position, Time.deltaTime * speed);
+            Vector3 rand_way_point = new Vector3(Random.Range(-max_pos, max_pos), 0, Random.Range(-max_pos, max_pos));
+            float distance = Vector3.Distance(path.points[current_way_point_id].position + rand_way_point, transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, path.points[current_way_point_id].position + rand_way_point, Time.deltaTime * speed);
 
             Quaternion rotation = Quaternion.LookRotation(path.points[current_way_point_id].position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotation_speed);
